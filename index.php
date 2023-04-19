0aa5e1770b57d9741db6942541ff2f9592f1cf4b
juliano340 <juliano340@gmail.com>
2023-04-19 20:48:12 -0300

    Começando!

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema Agendador 1.0</title>
+</head>
+<body>
+    <header>
+        <h1>Sistema agendador 1.0</h1>
+        <nav>
+            <a href="#">Home</a>
+            <a href="#">Contato</a>
+            <a href="#">Tarefas</a>
+            <a href="#">Eventos</a>
+        </nav>
+    </header>
+
+    <main>
+        <?php 
+            include  '/paginas/home/home.php';
+        
+        ?>
+    </main>
+    
+</body>
+</html>
