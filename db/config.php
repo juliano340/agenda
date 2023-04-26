86fb5237aca67a50e2fcbcadfdc045b6b2d5d848
juliano340 <juliano340@gmail.com>
2023-04-25 23:42:47 -0300

    Atualizand files db

--- db/config.php
+++ db/config.php
@@ -7,11 +7,3 @@
 
 ?> -->
 
-<?php
-
-    define("SERVIDOR", "sql569.main-hosting.eu");
-    define("USUARIO", "u550388678_root");
-    define("SENHA", "!B1eoOU8n");
-    define("BANCO", "u550388678_dbagenda");
-
-?>
