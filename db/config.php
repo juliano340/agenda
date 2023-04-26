2d32b039126f87c3e92a540509db9964421efc5d
juliano340 <juliano340@gmail.com>
2023-04-25 23:39:45 -0300

    Atualizando files

--- db/config.php
+++ db/config.php
@@ -1,8 +1,17 @@
-<?php
+<!-- <?php
 
     define("SERVIDOR", "localhost");
     define("USUARIO", "root");
     define("SENHA", "");
     define("BANCO", "dbsisagendador");
+
+?> -->
+
+<?php
+
+    define("SERVIDOR", "sql569.main-hosting.eu");
+    define("USUARIO", "u550388678_root");
+    define("SENHA", "!B1eoOU8n");
+    define("BANCO", "u550388678_dbagenda");
 
 ?>
