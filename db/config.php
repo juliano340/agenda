d92bf637d85d13c105e431aca86cd3b3f7508824
juliano340 <juliano340@gmail.com>
2023-04-25 23:58:21 -0300

    Ajustando config.php

--- db/config.php
+++ db/config.php
@@ -1,9 +1,22 @@
 <?php
 
+$tipo_conexao = $_SERVER['HTTP_HOST'];
+if($tipo_conexao = 'juliano340.com') {
+
+    define("SERVIDOR", "sql569.main-hosting.eu");
+    define("USUARIO", "u550388678_root");
+    define("SENHA", "!B1eoOU8n");
+    define("BANCO", "u550388678_dbagenda");
+
+
+} else {
+
+
     define("SERVIDOR", "localhost");
     define("USUARIO", "root");
     define("SENHA", "");
     define("BANCO", "dbsisagendador");
+}
 
 ?>
 
