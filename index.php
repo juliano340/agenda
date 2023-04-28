5473de1b2a26b478e42577fbe4bff3a18dca90e2
juliano340 <juliano340@gmail.com>
2023-04-28 00:33:48 -0300

    Inserindo bootstrap + footer

--- index.php
+++ index.php
@@ -10,20 +10,42 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
     <title>Sistema Agendador 1.0</title>
+    <link rel="stylesheet" href="css/estilo-padrao.css">
 </head>
 <body>
-    <header>
-        <h1>Sistema agendador 1.0</h1>
-        <nav>
-            <a href="index.php?menuop=home">Home</a>
-            <a href="index.php?menuop=contatos">Contato</a>
-            <a href="index.php?menuop=tarefas">Tarefas</a>
-            <a href="index.php?menuop=eventos">Eventos</a>
-        </nav>
+    <header class="bg-dark">
+        <div class="container">
+            <h1>Sistema agendador 1.0</h1>
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <a href="#" class="navbar-brand">
+                    <h2>Agenda Logotipo</h2>
+                </a>
+                <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active"><a class="nav-link" href="index.php?menuop=home">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="index.php?menuop=contatos">Contato</a></li>
+                    <li class="nav-item"><a class="nav-link" href="index.php?menuop=tarefas">Tarefas</a></li>
+                    <li class="nav-item"><a class="nav-link" href="index.php?menuop=eventos">Eventos</a></li>
+                </div>
+                
+                
+                
+            </nav>
+            
+                    
+            </ul>
+
+        </div>
+
     </header>
 
     <main>
+        <div class="container">
+
+        
         <?php 
             $menuop = (isset($_GET['menuop']))?$_GET['menuop']:'home';
             switch ($menuop) {
@@ -68,7 +90,12 @@
             }
         
         ?>
+        </div>
     </main>
+
+    <footer class="container-fluid fixed-bottom bg-dark">
+        <div class="text-center">SIS AGENDADOR v1.0 - juliano340.com</div>
+    </footer>
     
 </body>
 </html>
